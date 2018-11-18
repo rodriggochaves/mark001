fe581477ce39cada8adcbee2b3619cc47f92b462
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2018-11-18 13:17:02 -0200

    create module to control generations via conway rules

--- lib/mark001.ex
+++ lib/mark001.ex
@@ -1,18 +1,69 @@
 defmodule Mark001 do
-  @moduledoc """
-  Documentation for Mark001.
-  """
+  def create_board() do
+    [
+      %{x: 0, y: 0, cell: 'x'},
+      %{x: 0, y: 1, cell: 'x'},
+      %{x: 0, y: 2, cell: 'x'},
+      %{x: 1, y: 0, cell: 'x'},
+      %{x: 1, y: 1, cell: 'x'},
+      %{x: 1, y: 2, cell: 'x'},
+      %{x: 2, y: 0, cell: 'x'},
+      %{x: 2, y: 1, cell: 'x'},
+      %{x: 2, y: 2, cell: 'x'}
+    ]
+  end
 
-  @doc """
-  Hello world.
+  def get_cell(board, x, y) do
+    Enum.find(board, fn(tuple) -> tuple[:x] == x && tuple[:y] == y end)
+  end
 
-  ## Examples
+  def revive_cell(board, x, y) do
+    Enum.map(board, fn(tuple) -> update_cell(tuple, x, y, 'o') end)
+  end
 
-      iex> Mark001.hello()
-      :world
+  def kill_cell(board, x, y) do
+    Enum.map(board, fn(tuple) -> update_cell(tuple, x, y, 'x') end)
+  end
 
-  """
-  def hello do
-    :world
+  def check_conway_rules(cell) do
+    case {cell[:cell], cell[:neighbor]} do
+      {'x', 3}            -> update_cell(cell, cell[:x], cell[:y], 'o')
+      {'o', n} when n < 2 -> update_cell(cell, cell[:x], cell[:y], 'x')
+      {'o', n} when n > 3 -> update_cell(cell, cell[:x], cell[:y], 'x')
+      {_,_}               -> cell
+    end
+  end
+
+  def update_cell(tuple, x, y, new_state) do
+    case tuple do
+      %{x: ^x, y: ^y, cell: _} -> Map.merge(tuple, %{cell: new_state})
+      _                        -> tuple
+    end
+  end
+
+  def next_generation(board) do
+    count_neighbors(board) |>
+    Enum.map(fn(cell) -> check_conway_rules(cell) end)
+  end
+
+  def count_neighbors(board) do
+    Enum.map(board, fn(cell) -> Map.merge(cell, %{neighbor: count(board, cell)}) end)
+  end
+
+  def count(board, cell) do
+    [
+      %{x: cell[:x] - 1, y: cell[:y] - 1},
+      %{x: cell[:x] - 1, y: cell[:y]},
+      %{x: cell[:x] - 1, y: cell[:y] + 1},
+      %{x: cell[:x], y: cell[:y] - 1},
+      %{x: cell[:x], y: cell[:y] + 1},
+      %{x: cell[:x] + 1, y: cell[:y] - 1},
+      %{x: cell[:x] + 1, y: cell[:y]},
+      %{x: cell[:x] + 1, y: cell[:y] + 1}
+    ] 
+    |> Enum.filter(fn(neighbor) -> neighbor[:x] >= 0 || neighbor[:y] >= 0 end) 
+    |> Enum.map(fn(neighbor) -> get_cell(board, neighbor[:x], neighbor[:y])[:cell] == 'o' end)
+    |> Enum.filter(fn(x) -> x end)
+    |> Enum.count()    
   end
 end
--- test/mark001_test.exs
+++ test/mark001_test.exs
@@ -1,8 +1,86 @@
 defmodule Mark001Test do
   use ExUnit.Case
-  doctest Mark001
 
-  test "greets the world" do
-    assert Mark001.hello() == :world
+  test "#create_board a board of 9 dead cells" do
+    board = Mark001.create_board()
+    assert Enum.map(board, fn(cell) -> cell[:cell] end) == ['x','x','x','x','x','x','x','x','x']
+  end
+
+  test "#get_cell returns a cell" do
+    board = Mark001.create_board()
+    assert Mark001.get_cell(board, 1, 0) == %{x: 1, y: 0, cell: 'x'}
+  end
+
+  test "#revive_cell revice cell at (x,y)" do
+    board = Mark001.create_board() |>
+    Mark001.revive_cell(1, 0)
+    assert Mark001.get_cell(board, 1, 0) == %{x: 1, y: 0, cell: 'o'}
+  end
+
+  test "#update_cell with match cell its updated" do
+    cell = %{x: 1, y: 0, cell: 'o'}
+    assert Mark001.update_cell(cell, 1, 0, 'x') == %{x: 1, y: 0, cell: 'x'}
+  end
+
+  test "#update_cell without match cell its not updated" do
+    cell = %{x: 1, y: 0, cell: 'o'}
+    assert Mark001.update_cell(cell, 1, 1, 'x') == %{x: 1, y: 0, cell: 'o'}
+  end
+
+  test "#kill_cell" do
+    board = Mark001.create_board() |>
+    Mark001.revive_cell(1, 0) |>
+    Mark001.kill_cell(1, 0) 
+    assert Mark001.get_cell(board, 1, 0) == %{x: 1, y: 0, cell: 'x'}
+  end
+  
+  test "#next_generation any live cell with less than 2 neighbors dies" do
+    board = Mark001.create_board() |>
+    Mark001.revive_cell(0, 0) |>
+    Mark001.next_generation()
+    
+    assert Mark001.get_cell(board, 0, 0)[:cell] == 'x'
+  end
+
+  test "#next_generation any live cell with 2 or 3 neighbors lives" do
+    board = Mark001.create_board() |>
+    Mark001.revive_cell(0, 0) |>
+    Mark001.revive_cell(0, 1) |>
+    Mark001.revive_cell(0, 2) |>
+    Mark001.next_generation()
+    
+    assert Mark001.get_cell(board, 0, 1)[:cell] == 'o'
+  end
+
+  test "#next_generation any live cell with more than 3 neighbors dies" do
+    board = Mark001.create_board() |>
+    Mark001.revive_cell(0, 0) |>
+    Mark001.revive_cell(0, 1) |>
+    Mark001.revive_cell(0, 2) |>
+    Mark001.revive_cell(1, 0) |>
+    Mark001.revive_cell(1, 1) |>
+    Mark001.next_generation()
+    
+    assert Mark001.get_cell(board, 1, 1)[:cell] == 'x'
+  end
+
+  test "#next_generation any dead cell with 3 neighbors comes to live" do
+    board = Mark001.create_board() |>
+    Mark001.revive_cell(0, 0) |>
+    Mark001.revive_cell(1, 0) |>
+    Mark001.revive_cell(0, 1) |>
+    Mark001.next_generation()
+    
+    assert Mark001.get_cell(board, 1, 1)[:cell] == 'o'
+  end
+
+  test "#count_neighbors" do
+    neighbors = Mark001.create_board() |>
+    Mark001.revive_cell(0, 0) |>
+    Mark001.revive_cell(1, 0) |>
+    Mark001.revive_cell(0, 1) |>
+    Mark001.count_neighbors
+
+    assert Mark001.get_cell(neighbors, 1, 1)[:neighbor] == 3
   end
 end
